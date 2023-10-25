f0bf3f6eb20a0c6cb68e24b7333a0a7fcba8f04d
MyNameIsSyukra <sukrawahyu@gmail.com>
2023-10-25 15:29:38 +0700

    fix seeder

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::middleware('auth')->group(function () {
 Route::get('/form', [formController::class, 'index']);
 Route::post('/form', [formController::class, 'store']);
 Route::get('/result', [formController::class, 'result']);
-Route::get('/showall', [formController::class, 'showAll']);
+// Route::get('/showall', [formController::class, 'showAll']);
 
 require __DIR__.'/auth.php';
 
